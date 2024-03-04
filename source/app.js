88a1768ccab965dce37fa0918cfd844578701a5e
Waren Gonzaga <opensource@warengonzaga.com>
2024-03-05 00:58:45 +0800

    ✨: add openai api key handling

--- source/app.js
+++ source/app.js
@@ -4,9 +4,23 @@ import BigText from 'ink-big-text';
 import Gradient from 'ink-gradient';
 import info from './utils/info.js';
 import askForCommitMessage from './utils/commit.js';
+import { getOpenAIKey, setOpenAIKey, deleteOPenAIKey } from './utils/api.js';
 
-export default function App() {
-	askForCommitMessage();
+export default function App({flags}) {
+	if(flags.setopenai) {
+		setOpenAIKey(flags.setopenai);
+	}
+	if(flags.delopenai) {
+		deleteOPenAIKey();
+	}
+	if (!getOpenAIKey()) {
+		console.log('Please provide an OpenAI API key.');
+		console.log('You can get one from https://platform.openai.com/account/api-keys')
+		console.log('Run `magicc --setopenai=<api-key>` to save your API key and try again.');
+	} else {
+		console.log('You have an OpenAI API key, you can now generate a commit message.');
+		askForCommitMessage();
+	}
 	return (
 		<>
 			<Gradient name='passion'>
