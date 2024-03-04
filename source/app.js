73a85ec19fa36a86c647c78d96298f2933202d8e
Waren Gonzaga <opensource@warengonzaga.com>
2024-03-04 23:35:13 +0800

    ✨ tweak: update commit handling

--- source/app.js
+++ source/app.js
@@ -1,20 +1,26 @@
 import React from 'react';
-import {Text} from 'ink';
+import {Text, Newline} from 'ink';
 import BigText from 'ink-big-text';
 import Gradient from 'ink-gradient';
-// import getGitDiff from './utils/openai.js';
 import info from './utils/info.js';
+import askForCommitMessage from './utils/commit.js';
 
 export default function App() {
-	// getGitDiff();
+	askForCommitMessage();
 	return (
 		<>
-			<Text color='yellow'>Note: Coming Soon... Follow @warengonzaga for updates</Text>
 			<Gradient name='passion'>
 				<BigText text='Magicc' />
 				<Text>You can do `magicc`, you can build anything that you desire. 🪄</Text>
 			</Gradient>
-			<Text>Version: <Text color='green'>{info('version')}</Text> | Author: <Text color='blue'>{info('author')}</Text></Text>
+			<Text>
+				Version: <Text color='green'>{info('version')}</Text> | 
+				Author: <Text color='blue'>{info('author')}</Text><Newline/>
+				<Text>
+					Need Help? <Text color="cyan">magicc --help</Text>
+				</Text><Newline/>
+				==================================================
+			</Text>
 		</>
 	);
 }
