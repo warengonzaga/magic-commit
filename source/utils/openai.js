4d6b01ab12b600474dd74ad2bfb7b323415c0392
Waren Gonzaga <opensource@warengonzaga.com>
2024-03-04 23:36:18 +0800

    ✨ tweak: update initGit function

--- source/utils/openai.js
+++ source/utils/openai.js
@@ -7,24 +7,72 @@ dotenv.config();
 
 const openai = new OpenAI(process.env.OPENAI_API_KEY);
 
-async function getGitDiff() {
+// remove any staged changes in git
+async function initGit() {
 	try {
-			const { stdout: gitDiff } = await execa('git', ['diff']);
-
-			const completion = await openai.chat.completions.create({
-				messages: [
-					{
-						role: "system",
-						content: config.prompt,
-					},
-					{ role: "user", content: gitDiff },
-				],
-				model: "gpt-3.5-turbo-0125"
-			});
-			console.log(completion.choices[0].message.content);
+		await execa('git', ['restore', '--staged', '.']);
 	} catch (error) {
-			console.error(error);
+		console.error(error);
 	}
 }
 
-export default getGitDiff;
+// git status to see if there are any changes
+// if there's any changes add the first file in the list of changes
+async function gitStatus() {
+	try {
+		const { stdout: status } = await execa('git', ['status', '--porcelain']);
+		if (status) {
+			// get the first file path in the list of changes
+			const lines = status.split('\n');
+			const filePaths = lines
+				.map(line => line.split(' ').slice(2).join(' ').trim())
+				.filter(filePath => filePath !== '')
+				.concat(lines
+					.filter(line => line.startsWith('??'))
+					.map(line => line.split(' ').slice(1).join(' ').trim())
+				);
+			// git add the first file in the list of changes
+			await execa('git', ['add', filePaths[0]]);
+			console.log(`${filePaths[0]} has been added to the staging area.`);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+// get the diff of the staged changes
+async function gitDiff() {
+	try {
+		const { stdout: gitDiff } = await execa('git', ['diff', '--staged']);
+		return gitDiff;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+async function generatePrompt() {
+	// get the staged changes
+	await initGit();
+	await gitStatus();
+	const gitDiffContent = await gitDiff();
+
+	// use the prompt from the config file emoji and send to openai
+	const category = await openai.chat.completions.create({
+		messages: [
+			{role: "system", content: config.emoji},
+			{role: "user", content: gitDiffContent},
+		],
+		model: "gpt-3.5-turbo"
+	});
+	// use the prmopt from the config file message and send to openai
+	const message = await openai.chat.completions.create({
+		messages: [
+			{role: "system", content: config.message},
+			{role: "user", content: gitDiffContent},
+		],
+		model: "gpt-3.5-turbo"
+	});
+	return `${category.choices[0].message.content}: ${message.choices[0].message.content}`;
+}
+
+export default generatePrompt;
