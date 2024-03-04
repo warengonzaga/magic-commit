1f40e5a2c74797c3f6b302d9643e500df7538d71
Waren Gonzaga <opensource@warengonzaga.com>
2024-03-05 01:54:30 +0800

    ✨ tweak: update commit.js to handle cases without changes.

--- source/utils/commit.js
+++ source/utils/commit.js
@@ -10,21 +10,26 @@ async function askForCommitMessage() {
     output: process.stdout,
   });
 
-  rl.question(`Suggested commit message: ${prompt}\nDo you want to proceed? (y/n) `, (answer) => {
-    if (answer.toLowerCase() === "y") {
-      execa("git", ["commit", "-m", prompt])
-        .then(() => {
-          console.log("Changes committed successfully!");
-        })
-        .catch((error) => {
-          console.error("Failed to commit changes:", error);
-        });
-    } else {
-      console.log("Changes not committed.");
-    }
+  if (prompt) {
+    rl.question(`Suggested commit message: ${prompt}\nDo you want to proceed? (y/n) `, (answer) => {
+      if (answer.toLowerCase() === "y") {
+        execa("git", ["commit", "-m", prompt])
+          .then(() => {
+            console.log("Changes committed successfully!");
+          })
+          .catch((error) => {
+            console.error("Failed to commit changes:", error);
+          });
+      } else {
+        console.log("Changes not committed.");
+      }
 
+      rl.close();
+    });
+  } else {
+    console.log("No changes to commit...");
     rl.close();
-  });
+  }
 }
 
 export default askForCommitMessage;
